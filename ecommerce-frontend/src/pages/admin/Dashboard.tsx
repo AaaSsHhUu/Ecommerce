3663b47c0c7d736eb1d979bf3dcc11300f1deb6f
Ashu Negi <anegi6723@gmail.com>
2024-06-11 05:47:21 +0530

    using adminsidebar in dashboard

--- ecommerce-frontend/src/pages/admin/Dashboard.tsx
+++ ecommerce-frontend/src/pages/admin/Dashboard.tsx
@@ -1,7 +1,15 @@
+import { AdminSidebar } from "../../components"
+
 const Dashboard = () => {
   return (
-    <div>
-      dashboard
+    <div className="adminContainer">
+        {/* Sidebar */}
+        <AdminSidebar />
+
+        {/* Main */}
+        <main>
+          main  
+        </main>
     </div>
   )
 }
