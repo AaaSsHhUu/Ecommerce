b9fc78733a345c543c0f8d45131d62d642be899d
Ashu Negi <anegi6723@gmail.com>
2024-06-12 19:35:05 +0530

    made dashboard widgets

--- ecommerce-frontend/src/pages/admin/Dashboard.tsx
+++ ecommerce-frontend/src/pages/admin/Dashboard.tsx
@@ -1,16 +1,71 @@
+import { FaRegBell, FaSearch } from "react-icons/fa"
 import { AdminSidebar } from "../../components"
+import userImg from "../../assets/images/user.png";
+import { HiTrendingDown, HiTrendingUp } from "react-icons/hi";
 
 const Dashboard = () => {
+
   return (
     <div className="adminContainer">
-        {/* Sidebar */}
-        <AdminSidebar />
+      {/* Sidebar */}
+      <AdminSidebar />
 
-        {/* Main */}
-        <main>
-          main  
-        </main>
+      {/* Main */}
+      <main className="dashboard">
+        <div className="bar">
+          <FaSearch />
+          <input type="text" placeholder="Search for data, users, docs" />
+          <FaRegBell size={20} />
+          <img src={userImg} alt="user" />
+        </div>
+
+        <section className="widget-container">
+          <Widget percent={40} amount={true} value={340000} heading="Revenue" color="rgb(0,115,255)" />
+
+          <Widget percent={-14} value={400} heading="Users" color="rgb(0,198,202)" />
+          
+          <Widget percent={80} value={30000} heading="Transactions" color="rgb(255,196,0)" />
+          
+          <Widget percent={30} value={1000} heading="Products" color="rgb(75,0,255)" />
+        </section>
+      </main>
     </div>
+  )
+}
+
+interface widgetProps {
+  heading: string;
+  value: number;
+  percent: number;
+  color: string;
+  amount?: boolean;
+}
+
+const Widget = ({ heading, value, percent, color, amount }: widgetProps) => {
+  return (
+    <article className="widget">
+      <div className="widget-info">
+        <p>{heading}</p>
+        <h4>{amount ? `$${value}` : value}</h4>
+        {
+          percent > 0 ?
+            <span className="green">
+              <HiTrendingUp /> +{percent}%{" "}
+            </span>
+            :
+            <span className="red">
+              <HiTrendingDown /> {percent}%{" "}
+            </span>
+        }
+      </div>
+
+      <div className="widget-circle"
+        style={{
+          background : `conic-gradient(${color} ${(Math.abs(percent)/100) * 360}deg, white 0 )`
+        }}>
+        <span style={{color : color}}>{percent}%</span>
+      </div>
+    </article>
   )
 }
 
