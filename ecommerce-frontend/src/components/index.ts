e946094e6a1ba89d234e3a6599e353d1a830fed7
Ashu Negi <anegi6723@gmail.com>
2024-06-15 06:49:06 +0530

    Exporting Table

--- ecommerce-frontend/src/components/index.ts
+++ ecommerce-frontend/src/components/index.ts
@@ -1,6 +1,7 @@
 import AdminSidebar from "./AdminSidebar";
 import {BarChart,DoughnutChart} from "./Chart";
 import Loader from "./Loader";
+import Table from "./DashboardTable"
 
 
-export {Loader, AdminSidebar, BarChart, DoughnutChart}
+export {Loader, AdminSidebar, BarChart, DoughnutChart, Table}
