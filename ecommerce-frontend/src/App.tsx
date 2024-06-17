db4337b2eb0bfa30b78f39b790e90aa0c77d4329
Ashu Negi <anegi6723@gmail.com>
2024-06-17 13:01:02 +0530

    added routes of new managment pages

--- ecommerce-frontend/src/App.tsx
+++ ecommerce-frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Suspense } from "react";
-import { Cart, Customer, Dashboard, Home, Product, Search, Transactions } from "./pages";
+import { Cart, Customer, Dashboard, Home, NewProduct, Product, ProductManagment, Search, TransactionManagment, Transactions } from "./pages";
 import { Loader } from "./components";
 
 function App() {
@@ -17,6 +17,16 @@ function App() {
           <Route path="/admin/products" element={<Product />} />
           <Route path="/admin/transactions" element={<Transactions />} />
           <Route path="/admin/customers" element={<Customer />} />
+
+          {/* Charts */}
+
+          {/* Apps */}
+
+          {/* Managment */}
+          <Route path="/admin/products/new" element={<NewProduct />} />
+          <Route path="/admin/products/:id" element={<ProductManagment />} />
+          <Route path="/admin/transactions/:id" element={<TransactionManagment />} />
+
         </Routes>
       </Suspense>
     </Router>
--- ecommerce-frontend/src/pages/index.ts
+++ ecommerce-frontend/src/pages/index.ts
@@ -7,6 +7,9 @@ const Dashboard = lazy(() => import("./admin/Dashboard"));
 const Product = lazy(() => import("./admin/Product"));
 const Transactions = lazy(() => import("./admin/Transactions"));
 const Customer = lazy(() => import("./admin/Customer"));
+const NewProduct = lazy(() => import("./admin/managment/NewProduct"));
+const ProductManagment = lazy(() => import("./admin/managment/ProductManagment"));
+const TransactionManagment = lazy(() => import("./admin/managment/TransactionManagment"));
 
-export {Home, Cart, Search,Dashboard, Product, Transactions, Customer};
+export {Home, Cart, Search,Dashboard, Product, Transactions, Customer, NewProduct, ProductManagment, TransactionManagment };
 
