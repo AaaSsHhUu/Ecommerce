6613c42b021e2e6e0c42ce2843ccb1d19ccfba49
Ashu Negi <anegi6723@gmail.com>
2024-06-17 06:59:50 +0530

    made transactions page using TableHOC

--- ecommerce-frontend/src/pages/admin/Transactions.tsx
+++ ecommerce-frontend/src/pages/admin/Transactions.tsx
@@ -1,6 +1,58 @@
+import { ReactElement, useState, useCallback } from "react";
 import { AdminSidebar } from "../../components"
+import { Column } from "react-table";
+import TableHOC from "../../components/TableHOC";
 
 const Transactions = () => {
+  interface DataType{
+    user : string;
+    amount : number;
+    discount : number;
+    quantity : number;
+    status : string;
+    action : ReactElement;
+  }
+
+  const arr : DataType[] = [];
+
+  const [data] = useState<DataType[]>(arr);
+
+  const columns : Column<DataType>[] = [
+    {
+      Header : "Users",
+      accessor : "user"
+    },
+    {
+      Header : "Amount",
+      accessor : "amount"
+    },
+    {
+      Header : "Discount",
+      accessor : "discount"
+    },
+    {
+      Header : "Quantity",
+      accessor : "quantity"
+    },
+    {
+      Header : "Status",
+      accessor : "status"
+    },
+    {
+      Header : "Action",
+      accessor : "action"
+    },
+  ]
+
+  const Table = useCallback(
+    TableHOC(
+      columns,
+      data,
+      "dashboard-product-box", // same style -> same name
+      "Transactions",
+      data.length > 6
+    ),[])
+
   return (
     <div className="adminContainer">
         {/* Sidebar */}
@@ -8,7 +60,7 @@ const Transactions = () => {
 
         {/* Main */}
         <main>
-          main  
+          {Table()}  
         </main>
     </div>
   )
