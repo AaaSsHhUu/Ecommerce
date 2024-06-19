846a8057dcc14ab1cd53f4a685c1929ddf3b40aa
Ashu Negi <anegi6723@gmail.com>
2024-06-19 21:54:43 +0530

    created charts in BarCharts

---
+++ ecommerce-frontend/src/pages/admin/charts/BarCharts.tsx
@@ -0,0 +1,55 @@
+import { AdminSidebar, BarChart } from "../../../components"
+
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December"
+]
+const BarCharts = () => {
+  return (
+    <>
+      <div className="admin-container">
+        <AdminSidebar />
+        <main className="chart-container">
+            <h1>Bar Charts</h1>
+            <section>
+              <BarChart 
+                data1={[200,444,356,764,565,408,908]} 
+                data2={[345,104,412,683,250,786,167]} 
+                title1="Products" 
+                title2="Users" 
+                bgColor1="hsl(260,50%,30%)" 
+                bgColor2="hsl(360,90%,90%)" 
+              />
+              <h2>Top Selling Products & Top Customers</h2>
+            </section>
+
+            <section>
+              <BarChart 
+                data1={[200,444,356,764,565,408,908,345,104,412,683,250,786,167]} 
+                data2={[]} 
+                title1="Products" 
+                title2="" 
+                bgColor1="hsl(260,50%,30%)" 
+                bgColor2=""
+                labels={months} 
+                horizontal={true}
+              />
+              <h2>Orders Throughout the Year</h2>
+            </section>
+        </main>
+      </div>
+    </>
+  )
+}
+
+export default BarCharts
