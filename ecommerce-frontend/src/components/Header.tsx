f2c934a3660bb941c3cf087811155a464827e816
Ashu Negi <anegi6723@gmail.com>
2024-06-23 21:44:30 +0530

    added logo

--- ecommerce-frontend/src/components/Header.tsx
+++ ecommerce-frontend/src/components/Header.tsx
@@ -1,9 +1,10 @@
 import { useState } from "react"
-import { FaSearch, FaShoppingBag, FaSignInAlt, FaSignOutAlt, FaUser } from "react-icons/fa"
+import { FaHome, FaSearch, FaShoppingBag, FaSignInAlt, FaSignOutAlt, FaUser, FaUserCircle } from "react-icons/fa"
+import { IoLogoBitbucket } from "react-icons/io"
 import { Link } from "react-router-dom"
 
 const Header = () => {
-    const users = { _id : "djd", role : "admin" }
+    const users = { _id: "djd", role: "admin" }
 
     const [isOpen, setIsOpen] = useState(false);
 
@@ -11,47 +12,55 @@ const Header = () => {
         setIsOpen(false);
         // log out logic
     }
-  return (
-    <nav className="header">
-        <Link to={"/"} onClick={() => setIsOpen(false)}>Home</Link>
-        <Link to={"/search"} onClick={() => setIsOpen(false)}>
-            <FaSearch />
-        </Link>
-        <Link to={"/cart"} onClick={() => setIsOpen(false)}>
-            <FaShoppingBag />
-        </Link>
-        {
-            users?._id ?  // means user is logged in
-            (
-                <>
-                    <button onClick={() => setIsOpen(prev => !prev)}>
-                        <FaUser />
-                    </button>
-                    <dialog open={isOpen}>
-                        <div>
-                            {
-                                users?.role === "admin" && (
-                                    <Link to={"/admin/dashboard"} onClick={() => setIsOpen(false)}>Admin</Link>
-                                )
-                            }
-
-                            <Link to={"/orders"}>Orders</Link>
-                            <button onClick={logoutHandler}>
-                                <FaSignOutAlt />
-                            </button>
-                        </div>
-                    </dialog>
-                </>
-            )
-             : 
-            (
-                <Link to={"/login"}>
-                    <FaSignInAlt />
+    return (
+        <nav className="header">
+            <div className="logo">
+                <IoLogoBitbucket />
+                <h1>BuyBucket</h1>
+            </div>
+            <div className="nav-links">
+                <Link to={"/"} onClick={() => setIsOpen(false)}>
+                    <FaHome size={"1.3rem"} />Home
                 </Link>
-            )
-        }
-    </nav>
-  )
+                <Link to={"/search"} onClick={() => setIsOpen(false)}>
+                    <FaSearch />Search
+                </Link>
+                <Link to={"/cart"} onClick={() => setIsOpen(false)}>
+                    <FaShoppingBag />Cart
+                </Link>
+                {
+                    users?._id ?  // means user is logged in
+                        (
+                            <>
+                                <button onClick={() => setIsOpen(prev => !prev)}>
+                                    <FaUserCircle size={"1.3rem"}/>
+                                </button>
+                                <dialog open={isOpen}>
+                                    <div>
+                                        {
+                                            users?.role === "admin" && (
+                                                <Link to={"/admin/dashboard"} onClick={() => setIsOpen(false)}>Admin</Link>
+                                            )
+                                        }
+
+                                        <Link to={"/orders"}>Orders</Link>
+                                        <button onClick={logoutHandler}>
+                                            <FaSignOutAlt />
+                                        </button>
+                                    </div>
+                                </dialog>
+                            </>
+                        )
+                        :
+                        (
+                            <Link to={"/login"}>
+                                <FaSignInAlt />
+                            </Link>
+                        )
+                }
+            </div>
+        </nav>
+    )
 }
 
 export default Header
