c7e1ed9de2a93f4cc8553ad949608f7e15d9dec4
Ashu Negi <anegi6723@gmail.com>
2024-06-25 06:18:32 +0530

    modified login page form

--- ecommerce-frontend/src/pages/Login.tsx
+++ ecommerce-frontend/src/pages/Login.tsx
@@ -1,52 +1,73 @@
 import { TextField } from "@mui/material";
 import { ChangeEvent, useState } from "react";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+import { FcGoogle } from "react-icons/fc";
 
-interface LoginInfoProps{
-    email : string;
-    password : string;
+interface LoginInfoProps {
+    email: string;
+    password: string;
 }
 
 const Login = () => {
     const [loginInfo, setLoginInfo] = useState<LoginInfoProps>({
-        email : "",
+        email: "",
         password: ""
     })
+    const [showPassword,setShowPassword] = useState<boolean>(false);
 
-    const handleChange = (e : ChangeEvent<HTMLInputElement>) => {
-        setLoginInfo({...loginInfo, [e.target.name] : e.target.value}) 
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setLoginInfo({ ...loginInfo, [e.target.name]: e.target.value })
     }
-    return (
 
-        <div className="login">
-            <form>
-                {/* Email */}
-                <div>
-                    <TextField id="outlined-basic"
-                        name="email"
-                        label="Email"
-                        variant="outlined"
-                        value={loginInfo.email}
-                        sx={{ width: "100%" }}
-                        type="text"
-                        onChange={handleChange}
-                        required
-                    />
-                </div>
-                {/* Password */}
-                <div>
-                    <TextField id="outlined-basic"
-                        name="password"
-                        label="password"
-                        variant="outlined"
-                        value={loginInfo.password}
-                        sx={{ width: "100%" }}
-                        type="text"
-                        onChange={handleChange}
-                        required
-                    />
-                </div>
-            </form>
-        </div>
+
+    return (
+        <>
+            <div className="login">
+                <h1>Login</h1>
+                <form>
+                    {/* Email */}
+                    <div>
+                        <TextField id="outlined-basic"
+                            name="email"
+                            label="Email"
+                            variant="outlined"
+                            value={loginInfo.email}
+                            sx={{ width: "100%" }}
+                            type="text"
+                            onChange={handleChange}
+                            required
+                        />
+                    </div>
+                    {/* Password */}
+                    <div>
+                        <TextField id="outlined-basic"
+                            name="password"
+                            label="password"
+                            variant="outlined"
+                            value={loginInfo.password}
+                            sx={{ width: "100%" }}
+                            type={showPassword ? "text" : "password"}
+                            onChange={handleChange}
+                            required
+                        />
+                        <button type="button" onClick={() => setShowPassword(prev => !prev)}>
+                            {!showPassword ? <FaEyeSlash /> : <FaEye />}
+                        </button>
+                    </div>
+
+                    <button type="submit" className="login-btn">Login</button>
+
+                    <hr style={{width : "80%"}} />
+                    <div className="login-links">
+                        <div className="google-link">
+                            <FcGoogle /> 
+                            <p>Sign in using Google</p>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </>
     )
 }
 
