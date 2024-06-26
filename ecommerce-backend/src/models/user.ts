3cc1dcdaf79ec3352df11b5a3f7519ef648ac1f3
Ashu Negi <anegi6723@gmail.com>
2024-06-26 20:15:35 +0530

    used validator and added age virtual and interface in userSchema

--- ecommerce-backend/src/models/user.ts
+++ ecommerce-backend/src/models/user.ts
@@ -1,4 +1,19 @@
 import mongoose from "mongoose";
+import validator from "validator";
+
+interface IUser extends Document{
+    _id : string;
+    photo : string;
+    role : "user" | "admin";
+    name : string;
+    gender : "male" | "female";
+    dob : Date;
+    email : string;
+    createdAt : Date;
+    updatedAt : Date;
+    // virtual Attribute
+    age : number;
+}
 
 const userSchema = new mongoose.Schema({
     _id : {
@@ -29,9 +44,25 @@ const userSchema = new mongoose.Schema({
     },
     email : {
         type : String,
-        required : [true, "Please enter your email"]
+        unique : [true,"Email already exist"],
+        required : [true, "Please enter your email"],
+        validator : validator.default.isEmail
     },
 },{timestamps : true})
 
-const User = mongoose.model("User",userSchema);
+
+userSchema.virtual("age").get(function(this : IUser){
+    const today = new Date();
+    const dob = this.dob;
+
+    let age = today.getFullYear() - dob.getFullYear();
+    if(today.getMonth() < dob.getMonth() ||
+     (today.getMonth() === dob.getMonth() && today.getDate() < dob.getDate())
+    ){
+        age--;
+    }
+    return age;
+})
+
+const User = mongoose.model<IUser>("User",userSchema);
 export default User;
