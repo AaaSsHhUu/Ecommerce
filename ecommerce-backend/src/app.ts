15e55d38569b13e7423bebaba5934b9c505c6440
Ashu Negi <anegi6723@gmail.com>
2024-06-27 16:26:43 +0530

    used userRoutes in app.js

--- ecommerce-backend/src/app.ts
+++ ecommerce-backend/src/app.ts
@@ -1,7 +1,12 @@
 import express from "express";
+import useRoutes from "./routes/user.js";
 
 const app = express();
 const port = 4000;
+
+
+// Using Routes
+app.use("/api/v1/user",useRoutes);
 
 app.listen(port,()  => {
     console.log(`Server is listening on port ${port}`);
