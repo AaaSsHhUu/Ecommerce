668d71c6e44ab1a0c614f61e18aa728b4c7d1c2f
Ashu Negi <anegi6723@gmail.com>
2024-06-27 18:16:40 +0530

    used connectDB , and added root route and express.json middleware

--- ecommerce-backend/src/app.ts
+++ ecommerce-backend/src/app.ts
@@ -1,10 +1,19 @@
 import express from "express";
 import useRoutes from "./routes/user.js";
+import { connectDB } from "./utils/features.js";
 
 const app = express();
 const port = 4000;
 
+// Database connection
+connectDB();
 
+// middlewares
+app.use(express.json());
+
+app.get("/api/v1/", (req,res) => {
+    res.send("This is root route")
+})
 // Using Routes
 app.use("/api/v1/user",useRoutes);
 
