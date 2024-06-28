0ea534882634215eaad2e6856128589d7868ba09
Ashu Negi <anegi6723@gmail.com>
2024-06-28 18:22:45 +0530

    wrapped createdUser inside asyncHandler

--- ecommerce-backend/src/controller/user.ts
+++ ecommerce-backend/src/controller/user.ts
@@ -1,29 +1,28 @@
 import { Request, Response, NextFunction } from "express";
 import User, { UserSchemaValidation } from "../models/user.js";
 import ErrorHandler from "../utils/errorHandler.js";
+import { asyncHandler } from "../middleware/error.js";
 
-export const createUser = async (req: Request,res: Response,next: NextFunction) => {
-    const userBody = req.body;
-    // console.log("user body : ", userBody);
-    try{
+export const createUser = asyncHandler(
+    async (req: Request, res: Response, next: NextFunction) => {
+        const userBody = req.body;
+    
         // Zod validation check
         const {success} = UserSchemaValidation.safeParse(userBody);
         console.log("zod success : ",success);
-        
+            
         if(!success){
             throw new ErrorHandler("Invalid Input",400);
         }
-
+    
         const user = await User.create(userBody);
         if(!user){
             throw new ErrorHandler("Some error occured while creating user",500);
         }
-
-        res.status(201).json({
+    
+        return res.status(201).json({
             success : true,
             user
         })
-    }catch(err){
-        next(err);
     }
-};
+)
