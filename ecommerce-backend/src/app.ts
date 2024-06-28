66098216e78e2afe2e56641e53cea6b54a1ec0d8
Ashu Negi <anegi6723@gmail.com>
2024-06-28 17:25:24 +0530

    used errorMiddleware

--- ecommerce-backend/src/app.ts
+++ ecommerce-backend/src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import useRoutes from "./routes/user.js";
 import { connectDB } from "./utils/features.js";
+import errorMiddleware from "./middleware/error.js";
 
 const app = express();
 const port = 4000;
@@ -16,6 +17,10 @@ app.get("/api/v1/", (req,res) => {
 })
 // Using Routes
 app.use("/api/v1/user",useRoutes);
+
+// Error handling middleware
+app.use(errorMiddleware);
+
 
 app.listen(port,()  => {
     console.log(`Server is listening on port ${port}`);
