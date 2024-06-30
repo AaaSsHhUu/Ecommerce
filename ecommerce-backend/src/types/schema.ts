1721bf5c12e2597e524b4a8f0a4912e9cf98b1a5
Ashu Negi <anegi6723@gmail.com>
2024-06-30 14:44:18 +0530

    created separate schema file for zod validations

---
+++ ecommerce-backend/src/types/schema.ts
@@ -0,0 +1,20 @@
+import {z} from "zod";
+
+export const signupValidation = z.object({
+    photo : z.string().min(1,"Please Add one photo"),
+    role : z.enum(["admin","user"]).default("user"),
+    name : z.string().min(1,"Please Enter name"),
+    email : z.string().email("Invalid Email format"),
+    password : z.string().min(8, "Password should atleast have 8 letters"),
+    gender : z.enum(["male", "female"]),
+    dob : z.string().or(z.date()).transform(val => new Date(val)),
+})
+
+export const signinValidation = z.object({
+    email : z.string().email("Enter valid email address"),
+    password : z.string()
+})
+
+// type inference
+export type SignupInput = z.infer<typeof signupValidation>
+export type SigninInput = z.infer<typeof signinValidation>
