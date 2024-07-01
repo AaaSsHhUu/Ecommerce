0d61047d47f0f6a0f014f5c024cbaeaea0814508
Ashu Negi <anegi6723@gmail.com>
2024-07-01 12:47:18 +0530

    added route for deleting user

--- ecommerce-backend/src/routes/admin.ts
+++ ecommerce-backend/src/routes/admin.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import {getAllUsers, getUser} from "../controller/admin.js";
+import {deleteUser, getAllUsers, getUser} from "../controller/admin.js";
 import { isAuthenticated, isAuthorized } from "../middleware/auth.js";
 
 const router = Router();
@@ -7,5 +7,7 @@ const router = Router();
 // Admin routes
 router.get("/users",isAuthenticated, isAuthorized("admin"),getAllUsers);
 router.get("/user/:id",isAuthenticated, isAuthorized("admin"), getUser);
+router.delete("/user/:id",isAuthenticated,isAuthorized("admin"), deleteUser);
+
 
 export default router;
