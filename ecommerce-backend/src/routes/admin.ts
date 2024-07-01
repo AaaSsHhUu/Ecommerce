90fb4b23103f9fb99833da414b3a06cb521ce9dd
Ashu Negi <anegi6723@gmail.com>
2024-07-01 12:29:42 +0530

    used auth middlewares in admin routes

--- ecommerce-backend/src/routes/admin.ts
+++ ecommerce-backend/src/routes/admin.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import {getAllUsers, getUser} from "../controller/admin.js";
+import { isAuthenticated, isAuthorized } from "../middleware/auth.js";
 
 const router = Router();
 
 // Admin routes
-router.get("/users",getAllUsers);
-router.get("/user/:id", getUser);
+router.get("/users",isAuthenticated, isAuthorized("admin"),getAllUsers);
+router.get("/user/:id",isAuthenticated, isAuthorized("admin"), getUser);
 
 export default router;
