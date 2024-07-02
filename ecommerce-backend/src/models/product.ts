dbddfd73af1c91fa9ffd3e48e59b7251ee6411ee
Ashu Negi <anegi6723@gmail.com>
2024-07-02 11:09:24 +0530

    created product schema and model

---
+++ ecommerce-backend/src/models/product.ts
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+    name : {
+        type : String,
+        required : [true, "Please enter  product name"]
+    },
+    photo : {
+        type : String,
+        required : [true, "Please enter one photo"]
+    },
+    price : {
+        type : Number,
+        required : [true, "Enter Price"]
+    },
+    stock : {
+        type : Number,
+        required : [true, "Enter Stock"],
+    },
+    category : {
+        type : String,
+        required : [true, "Please Enter product category"]
+    }
+},{
+    timestamps : true
+})
+
+const Product = mongoose.model("Product", productSchema);
+export default Product;
