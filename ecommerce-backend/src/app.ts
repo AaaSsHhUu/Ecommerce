54c7c7687fd5fa23918cea2a77afa308915390a3
Ashu Negi <anegi6723@gmail.com>
2024-07-02 11:08:56 +0530

    removed admin routes and added product routes

--- ecommerce-backend/src/app.ts
+++ ecommerce-backend/src/app.ts
@@ -4,8 +4,10 @@ dotenv.config();
 import { connectDB } from "./utils/features.js";
 import errorMiddleware from "./middleware/error.js";
 import cookieParser from "cookie-parser";
-import adminRoutes from "./routes/admin.js"
+
+// Routes
 import userRoutes from "./routes/user.js";
+import productRoutes from "./routes/product.js";
 
 const app = express();
 const port = 4000;
@@ -21,10 +23,9 @@ app.get("/api/v1/", (req,res) => {
     res.send("This is root route")
 })
 // Using Routes
-app.use("/api/v1/user",userRoutes);
+app.use("/api/v1/user", userRoutes);
+app.use("/api/v1/product", productRoutes);
 
-// admin routes
-app.use("/api/v1/admin",adminRoutes);
 
 // Error handling middleware
 app.use(errorMiddleware);
