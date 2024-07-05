d94517b47816e884437406e18e9054c5857ee4b2
Ashu Negi <anegi6723@gmail.com>
2024-07-05 18:23:19 +0530

    added /search routes and  admin-products routes and documentation

--- ecommerce-backend/src/routes/product.ts
+++ ecommerce-backend/src/routes/product.ts
@@ -1,15 +1,26 @@
 import express,{Router} from "express";
-import { createProduct, deleteProduct, getAllCategories, getAllProducts, getLatestProducts, getOneProduct, updateProduct } from "../controller/product.js";
+import { createProduct, deleteProduct, getAdminProducts, getAllCategories, getAllProducts, getLatestProducts, getOneProduct, updateProduct } from "../controller/product.js";
 import { isAdmin, isAuthenticated } from "../middleware/auth.js";
 import { upload } from "../middleware/multer.js";
 
 const router = Router();
 
+// To create new product
 router.post("/new", isAuthenticated, isAdmin, upload.single("photo"), createProduct);
-router.get("/latest", getLatestProducts);
-router.get("/categories", getAllCategories);
-router.get("/all", getAllProducts);
 
+// To get recently added 10 products
+router.get("/latest", getLatestProducts);
+
+// To get all unique Categories
+router.get("/categories", getAllCategories);
+
+// To get all products with filter
+router.get("/search", getAllProducts);
+
+// To get all products for admin
+router.get("/admin-products",isAuthenticated, isAdmin, getAdminProducts )
+
+// To getOne, deleteOne, and updateOne
 router.route("/:id").get(getOneProduct).delete(isAuthenticated, isAdmin, deleteProduct).put(isAuthenticated,isAdmin,upload.single("photo") ,updateProduct);
 
 export default router;
