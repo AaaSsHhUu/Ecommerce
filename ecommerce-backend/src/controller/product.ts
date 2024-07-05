ba7cb26be65fd1c110372f914d693f54d03ea401
Ashu Negi <anegi6723@gmail.com>
2024-07-05 11:06:00 +0530

    added controller and route for update product, and formatted file

--- ecommerce-backend/src/controller/product.ts
+++ ecommerce-backend/src/controller/product.ts
@@ -1,135 +1,174 @@
 import { asyncHandler } from "../middleware/error.js";
-import {Request, Response, NextFunction} from "express";
+import { Request, Response, NextFunction } from "express";
 import { newProductValidation } from "../types/schema.js";
 import ErrorHandler from "../utils/errorHandler.js";
 import Product from "../models/product.js";
-import {rm} from "fs";
+import { rm } from "fs";
 
 export const createProduct = asyncHandler(
-    async (req : Request, res : Response, next : NextFunction) => {
-        const {
-            name,
-            price,
-            stock,
-            category
-        } = req.body;
-        const photo = req.file;
-        
-        const {success} = newProductValidation.safeParse(req.body);
-        
-        if(!photo){
-            throw new ErrorHandler("Please provide photo",400);
-        }
-        
-        if(!success){
-            rm(photo?.path,() => {
-                console.log("Image deleted");
-            })
-            throw new ErrorHandler("Invalid Inputs",400);
-        }
-        
-        const product = await Product.create({
-            name,
-            category : category.toLowerCase(),
-            stock : Number(stock),
-            price : Number(price),
-            photo : photo?.path
-        })
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { name, price, stock, category } = req.body;
+    const photo = req.file;
 
-        if(!product){
-            throw new ErrorHandler("Some error occured while creating product", 500);
-        }
+    const { success } = newProductValidation.safeParse(req.body);
 
-        return res.status(201).json({
-            success : true,
-            message : "Product created successfully",
-            product
-        })
+    if (!photo) {
+      throw new ErrorHandler("Please provide photo", 400);
     }
-)
+
+    if (!success) {
+      rm(photo?.path, () => {
+        console.log("Image deleted");
+      });
+      throw new ErrorHandler("Invalid Inputs", 400);
+    }
+
+    const product = await Product.create({
+      name,
+      category: category.toLowerCase(),
+      stock: Number(stock),
+      price: Number(price),
+      photo: photo?.path,
+    });
+
+    if (!product) {
+      throw new ErrorHandler("Some error occured while creating product", 500);
+    }
+
+    return res.status(201).json({
+      success: true,
+      message: "Product created successfully",
+      product,
+    });
+  }
+);
 
 // Get lastest Products
 export const getLatestProducts = asyncHandler(
-    async(req : Request, res : Response, next : NextFunction) => {
-        const products = await Product.find().sort({createdAt : -1}).limit(5);
+  async (req: Request, res: Response, next: NextFunction) => {
+    const products = await Product.find().sort({ createdAt: -1 }).limit(5);
 
-        if(!products){
-            throw new ErrorHandler("Some error occured while fetching products",500);
-        }
-
-        return res.status(200).json({
-            success : true,
-            products
-        })
+    if (!products) {
+      throw new ErrorHandler("Some error occured while fetching products", 500);
     }
-)
+
+    return res.status(200).json({
+      success: true,
+      products,
+    });
+  }
+);
 
 // Get all categories
 
 export const getAllCategories = asyncHandler(
-    async (req : Request, res : Response, next : NextFunction) => {
-        const categories = await Product.distinct("category");
+  async (req: Request, res: Response, next: NextFunction) => {
+    const categories = await Product.distinct("category");
 
-        if(!categories){
-            throw new ErrorHandler("No categories found", 404);
-        }
-
-        return res.status(200).json({
-            success : true,
-            categories
-        })
+    if (!categories) {
+      throw new ErrorHandler("No categories found", 404);
     }
-)
 
-// get all products 
+    return res.status(200).json({
+      success: true,
+      categories,
+    });
+  }
+);
+
+// get all products
 
 export const getAllProducts = asyncHandler(
-    async(req : Request, res : Response, next : NextFunction) => {
-        const products = await Product.find();
+  async (req: Request, res: Response, next: NextFunction) => {
+    const products = await Product.find();
 
-        if(!products){
-            throw new ErrorHandler("No Product found",404);
-        }
-
-        return res.status(200).json({
-            success : true,
-            products
-        })
+    if (!products) {
+      throw new ErrorHandler("No Product found", 404);
     }
-)
+
+    return res.status(200).json({
+      success: true,
+      products,
+    });
+  }
+);
 
 // getProduct by id
 export const getOneProduct = asyncHandler(
-    async(req : Request, res : Response, next : NextFunction) => {
-        const id = req.params.id;
+  async (req: Request, res: Response, next: NextFunction) => {
+    const id = req.params.id;
 
-        const product = await Product.findById(id);
-        
-        if(!product){
-            throw new ErrorHandler("Product not found", 404);
-        }
+    const product = await Product.findById(id);
 
-        return res.status(200).json({
-            success : true,
-            product
-        })
+    if (!product) {
+      throw new ErrorHandler("Product not found", 404);
     }
-)
+
+    return res.status(200).json({
+      success: true,
+      product,
+    });
+  }
+);
 
 // delete product
 export const deleteProduct = asyncHandler(
-    async(req : Request, res : Response, next : NextFunction) => {
-        const id = req.params.id;
-        const product = await Product.findByIdAndDelete(id);
+  async (req: Request, res: Response, next: NextFunction) => {
+    const id = req.params.id;
+    const product = await Product.findById(id);
 
-        if(!product){
-            throw new ErrorHandler("Some error occured while deleting product", 500);
-        }
-
-        return res.status(200).json({
-            success : true,
-            product
-        })
-
+    if (!product) {
+      throw new ErrorHandler("Invalid id, product not found", 404);
     }
-)
+
+    if (product) {
+      rm(product?.photo!, () => {
+        console.log("removed product photo");
+      });
+    }
+
+    await Product.deleteOne({ id });
+    return res.status(200).json({
+      success: true,
+      message: "Product deleted successfully",
+      product,
+    });
+  }
+);
+
+// update product
+export const updateProduct = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const id = req.params.id;
+
+    const { name, price, stock, category } = req.body;
+    const photo = req.file;
+
+    const product = await Product.findById(id);
+    // console.log("product : ",product);
+    
+    if (!product) {
+      throw new ErrorHandler("Invalid id, product not found", 404);
+    }
+
+    if (photo) {
+      rm(product?.photo!, () => {
+        console.log("removed product previous photo");
+      });
+      product.photo = photo.path
+    }
+
+    if (name) product.name = name;
+    if (price) product.price = price;
+    if (stock) product.stock = stock;
+    if (category) product.category = category;
+
+    const updatedProduct = await product.save();
+
+    return res.status(200).json({
+      success: true,
+      message: "Product updated successfully",
+      updatedProduct,
+    });
+  }
+);
--- ecommerce-backend/src/routes/product.ts
+++ ecommerce-backend/src/routes/product.ts
@@ -1,5 +1,5 @@
 import express,{Router} from "express";
-import { createProduct, deleteProduct, getAllCategories, getAllProducts, getLatestProducts, getOneProduct } from "../controller/product.js";
+import { createProduct, deleteProduct, getAllCategories, getAllProducts, getLatestProducts, getOneProduct, updateProduct } from "../controller/product.js";
 import { isAdmin, isAuthenticated } from "../middleware/auth.js";
 import { upload } from "../middleware/multer.js";
 
@@ -10,6 +10,6 @@ router.get("/latest", getLatestProducts);
 router.get("/categories", getAllCategories);
 router.get("/all", getAllProducts);
 
-router.route("/:id").get(getOneProduct).delete(isAuthenticated, isAdmin, deleteProduct)
+router.route("/:id").get(getOneProduct).delete(isAuthenticated, isAdmin, deleteProduct).put(isAuthenticated,isAdmin,upload.single("photo") ,updateProduct);
 
 export default router;
