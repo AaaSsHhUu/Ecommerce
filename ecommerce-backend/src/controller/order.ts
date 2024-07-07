2fc833dc5c8efa31b8fca88d8e6a4357f7774cb4
Ashu Negi <anegi6723@gmail.com>
2024-07-07 17:03:08 +0530

    created newOrder controller

--- ecommerce-backend/src/controller/order.ts
+++ ecommerce-backend/src/controller/order.ts
@@ -1,3 +1,37 @@
-export const newOrder = () => {
-    
-}
+import {Request, Response, NextFunction} from "express";
+import { asyncHandler } from "../middleware/error.js";
+import {newOrderSchema} from "../types/schema.js"
+import ErrorHandler from "../utils/errorHandler.js";
+import Order from "../models/order.js";
+import { invalidateCache, reduceStock } from "../utils/features.js";
+
+export const newOrder = asyncHandler(
+    async (req : Request, res : Response, next : NextFunction) => {
+        const {shippingInfo,orderItems, user, subTotal, tax, discount, total} = req.body;
+        const {success, error} = newOrderSchema.safeParse({shippingInfo,orderItems, user, subTotal, tax, discount, total});
+
+        if(!success){
+            console.log("zod Error : ", error);
+            throw new ErrorHandler("Invalid Inputs", 400);
+        }
+
+        const order = await Order.create({
+            shippingInfo,
+            orderItems,
+            user,
+            subTotal,
+            tax,
+            discount,
+            total
+        })
+
+        await reduceStock(orderItems);
+
+        await invalidateCache({ product:true, order : true, admin : true })
+
+        return res.status(201).json({
+            message : "Order Created successfully",
+
+        })
+    }
+)
