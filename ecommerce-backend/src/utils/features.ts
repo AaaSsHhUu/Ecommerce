dddc46563230192e33f6c35e5217ec3bc2a29329
Ashu Negi <anegi6723@gmail.com>
2024-07-07 17:03:38 +0530

    created method to reduce product stock when new order is placed

--- ecommerce-backend/src/utils/features.ts
+++ ecommerce-backend/src/utils/features.ts
@@ -2,22 +2,37 @@ import mongoose from "mongoose";
 import { InvalidateCacheProps } from "../types/types.js";
 import Product from "../models/product.js";
 import { myCache } from "../app.js";
+import { OrderItemType } from "../types/schema.js";
+import ErrorHandler from "./errorHandler.js";
 
-export const connectDB = async () => {
-    mongoose.connect("mongodb://localhost:27017/",{dbName : "Ecommerce"})
+export const connectDB = async (uri : string) => {
+    mongoose.connect(uri,{dbName : "Ecommerce"})
     .then((c) => console.log(`Connected to DB, host : ${c.connection.host}`))
     .catch((err) => console.log("Error while connecting DB : ",err))
 }
 
 export const invalidateCache = async ({product,order,admin} : InvalidateCacheProps) => {
-    const productKeys : string[] = ["latest-products", "categories", "admin-products"];
-
+    
     if(product){
+        const productKeys : string[] = ["latest-products", "categories", "admin-products"];
         const productsIds = await Product.find().select("_id");
         productsIds.forEach(i => {
             productKeys.push(`product-${i._id}`);
         })
         myCache.del(productKeys);
     }
+}
 
+export const reduceStock = async (orderItem : OrderItemType[]) => {
+    for(let i=0 ; i < orderItem.length ; i++){
+        let order = orderItem[i];
+        const product = await Product.findById(order.productId);
+
+        if(!product){
+            throw new ErrorHandler("Product not found", 404);
+        }
+
+        product.stock -= order.quantity;
+        await product.save();
+    }
 }
