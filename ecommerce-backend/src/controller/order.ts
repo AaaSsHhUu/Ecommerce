9dd03097a265df0f9facfbe3737aba5ba688e444
Ashu Negi <anegi6723@gmail.com>
2024-07-08 14:42:36 +0530

    added get all orders route and controller for admin

--- ecommerce-backend/src/controller/order.ts
+++ ecommerce-backend/src/controller/order.ts
@@ -1,6 +1,6 @@
-import {Request, Response, NextFunction} from "express";
+import { Request, Response, NextFunction } from "express";
 import { asyncHandler } from "../middleware/error.js";
-import {newOrderSchema} from "../types/schema.js"
+import { newOrderSchema } from "../types/schema.js";
 import ErrorHandler from "../utils/errorHandler.js";
 import Order from "../models/order.js";
 import { invalidateCache, reduceStock } from "../utils/features.js";
@@ -10,78 +10,120 @@ import { myCache } from "../app.js";
 
 // new order
 export const newOrder = asyncHandler(
-    async (req : Request, res : Response, next : NextFunction) => {
-        const {shippingInfo,orderItems, user, subTotal, shippingCharges , tax, discount, total} = req.body;
+  async (req: Request, res: Response, next: NextFunction) => {
+    const {
+      shippingInfo,
+      orderItems,
+      user,
+      subTotal,
+      shippingCharges,
+      tax,
+      discount,
+      total,
+    } = req.body;
 
-        console.log("req body : ", req.body);
-        
-        // Zod validation check
-        const {success, error} = newOrderSchema.safeParse({shippingInfo,orderItems, user, subTotal, shippingCharges, tax, discount, total});
+    console.log("req body : ", req.body);
 
-        if(!success){
-            throw new ErrorHandler(error.message, 400);
-        }
+    // Zod validation check
+    const { success, error } = newOrderSchema.safeParse({
+      shippingInfo,
+      orderItems,
+      user,
+      subTotal,
+      shippingCharges,
+      tax,
+      discount,
+      total,
+    });
 
-        // Checking if user id is valid
-        if(!mongoose.isValidObjectId(user)){
-            throw new ErrorHandler("Invalid user id", 400);
-        }
-
-        // checking if there exist a user with given id
-        const isUser = await User.findById(user);
-        if(!isUser){
-            throw new ErrorHandler("User doesn't exist", 400);
-        }
-
-        const order = await Order.create({
-            shippingInfo,
-            orderItems,
-            user,
-            subTotal,
-            shippingCharges,
-            tax,
-            discount,
-            total
-        })
-
-        if(!order){
-            throw new ErrorHandler("Some error occured while placing the order, Please try again", 500);
-        }
-
-        await reduceStock(orderItems);
-
-        await invalidateCache({ product:true, order : true, admin : true });
-
-        return res.status(201).json({
-            message : "Order Placed successfully",
-        })
+    if (!success) {
+      throw new ErrorHandler(error.message, 400);
     }
-)
 
+    // Checking if user id is valid
+    if (!mongoose.isValidObjectId(user)) {
+      throw new ErrorHandler("Invalid user id", 400);
+    }
 
-// my-order 
+    // checking if there exist a user with given id
+    const isUser = await User.findById(user);
+    if (!isUser) {
+      throw new ErrorHandler("User doesn't exist", 400);
+    }
+
+    const order = await Order.create({
+      shippingInfo,
+      orderItems,
+      user,
+      subTotal,
+      shippingCharges,
+      tax,
+      discount,
+      total,
+    });
+
+    if (!order) {
+      throw new ErrorHandler(
+        "Some error occured while placing the order, Please try again",
+        500
+      );
+    }
+
+    await reduceStock(orderItems);
+
+    await invalidateCache({ product: true, order: true, admin: true });
+
+    return res.status(201).json({
+      message: "Order Placed successfully",
+    });
+  }
+);
+
+// my-order
 export const myOrder = asyncHandler(
-    async (req : Request, res : Response, next : NextFunction) => {
-        const {id} = req.params;
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { id } = req.params;
 
-        const key = `my-order-${id}`;
+    const key = `my-order-${id}`;
 
-        let orders = [];
+    let orders = [];
 
-        if(myCache.has(key)){
-            orders = JSON.parse(myCache.get(key) as string);
-        }
-        else{
-            orders = await Order.find({user : id});
-            if(!orders){
-                throw new ErrorHandler("No orders found", 404);
-            }
-            myCache.set(key, JSON.stringify(orders));
-        }
-
-        return res.status(201).json({
-            success : true,
-            orders
-        })
+    if (myCache.has(key)) {
+      orders = JSON.parse(myCache.get(key) as string);
+    } else {
+      orders = await Order.find({ user: id });
+      if (!orders) {
+        throw new ErrorHandler("No orders found", 404);
+      }
+      myCache.set(key, JSON.stringify(orders));
     }
-)
+
+    return res.status(200).json({
+      success: true,
+      orders,
+    });
+  }
+);
+
+// All-orders -- Admin
+export const allOrders = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const key = "all-orders";
+
+    let allOrders = [];
+
+    if(myCache.has(key)){
+        allOrders = JSON.parse(myCache.get(key) as string);
+    }
+    else{
+        allOrders = await Order.find().populate("user","name");
+        myCache.set(key, JSON.stringify(allOrders));
+    }
+
+    return res.status(200).json({
+        success : true,
+        allOrders
+    })
+
+  }
+);
--- ecommerce-backend/src/routes/order.ts
+++ ecommerce-backend/src/routes/order.ts
@@ -1,10 +1,13 @@
 import express from "express";
-import { myOrder, newOrder } from "../controller/order.js";
+import { allOrders, myOrder, newOrder } from "../controller/order.js";
+import { isAdmin, isAuthenticated } from "../middleware/auth.js";
 
 const router = express.Router();
 
 router.post("/new", newOrder);
 
 router.get("/my-order/:id", myOrder);
+
+router.get("/all-orders",isAuthenticated, isAdmin, allOrders);
 
 export default router;
