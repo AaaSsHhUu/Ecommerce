49f5d616f56732caee043ede3c9c49ac98fa8b96
Ashu Negi <anegi6723@gmail.com>
2024-07-08 15:03:43 +0530

    handling cast error in error middleware

--- ecommerce-backend/src/middleware/error.ts
+++ ecommerce-backend/src/middleware/error.ts
@@ -6,6 +6,10 @@ const errorMiddleware = async (err : ErrorHandler, req : Request, res : Response
     err.message ||= "Some Error Occured";
     err.statusCode ||= 500;
 
+    if(err.name === "CastError"){
+        err.message = "Invalid ID"
+    }
+
     res.status(err.statusCode).json({
         message : err.message,
         error : err,
