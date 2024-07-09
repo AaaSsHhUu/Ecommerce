f642b9350ae02f7a19e7176034c3a82d5162710e
Ashu Negi <anegi6723@gmail.com>
2024-07-09 14:27:04 +0530

    modified invalidateCache method

--- ecommerce-backend/src/utils/features.ts
+++ ecommerce-backend/src/utils/features.ts
@@ -6,45 +6,58 @@ import { OrderItemType } from "../types/schema.js";
 import ErrorHandler from "./errorHandler.js";
 import Order from "../models/order.js";
 
-export const connectDB = async (uri : string) => {
-    mongoose.connect(uri,{dbName : "Ecommerce"})
+export const connectDB = async (uri: string) => {
+  mongoose
+    .connect(uri, { dbName: "Ecommerce" })
     .then((c) => console.log(`Connected to DB, host : ${c.connection.host}`))
-    .catch((err) => console.log("Error while connecting DB : ",err))
-}
+    .catch((err) => console.log("Error while connecting DB : ", err));
+};
 
-export const invalidateCache = async ({product,order,admin,userId} : InvalidateCacheProps) => {
-    
-    if(product){
-        const productKeys : string[] = ["latest-products", "categories", "admin-products"];
-        const productsIds = await Product.find().select("_id");
-        productsIds.forEach(i => {
-            productKeys.push(`product-${i._id}`);
-        })
-        myCache.del(productKeys);
+export const invalidateCache = async ({
+  product,
+  order,
+  admin,
+  userId,
+  orderId,
+  productId,
+}: InvalidateCacheProps) => {
+  if (product) {
+    const productKeys: string[] = [
+      "latest-products",
+      "categories",
+      "admin-products",
+    ];
+
+    if(typeof productId === "string"){
+      productKeys.push(`product-${productId}`)
+    }
+    if(typeof productId === "object"){
+        productId.forEach(i => productKeys.push(`product-${i}`))
     }
 
-    if(order){
-        const orderKeys : string[] = ["all-orders", `my-order-${userId}`];
-        const orders = await Order.find().select("_id");
+    myCache.del(productKeys);
+  }
 
-        orders.forEach((i) => {
-            orderKeys.push(`order-${i._id}`);
-        })
+  if (order) {
+    const orderKeys: string[] = [
+      "all-orders",
+      `my-order-${userId}`,
+      `order-${orderId}`,
+    ];
+    myCache.del(orderKeys);
+  }
+};
 
-        myCache.del(orderKeys);
+export const reduceStock = async (orderItem: OrderItemType[]) => {
+  for (let i = 0; i < orderItem.length; i++) {
+    let order = orderItem[i];
+    const product = await Product.findById(order.productId);
+
+    if (!product) {
+      throw new ErrorHandler("Product not found", 404);
     }
-}
 
-export const reduceStock = async (orderItem : OrderItemType[]) => {
-    for(let i=0 ; i < orderItem.length ; i++){
-        let order = orderItem[i];
-        const product = await Product.findById(order.productId);
-
-        if(!product){
-            throw new ErrorHandler("Product not found", 404);
-        }
-
-        product.stock -= order.quantity;
-        await product.save();
-    }
-}
+    product.stock -= order.quantity;
+    await product.save();
+  }
+};
