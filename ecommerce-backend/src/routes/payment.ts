1b69143e316308f91acadd13881366361da58eff
Ashu Negi <anegi6723@gmail.com>
2024-07-10 12:41:30 +0530

    added route for all coupons with auth middlewares

--- ecommerce-backend/src/routes/payment.ts
+++ ecommerce-backend/src/routes/payment.ts
@@ -1,9 +1,12 @@
 import express from "express";
-import { applyCoupon, newCoupon } from "../controller/payment.js";
+import { allCoupons, applyCoupon, newCoupon } from "../controller/payment.js";
+import { isAdmin, isAuthenticated } from "../middleware/auth.js";
 
 const router = express.Router();
 
 router.post("/coupon/new", newCoupon);
 router.post("/discount", applyCoupon);
+
+router.get("/coupon/all",isAuthenticated, isAdmin, allCoupons);
 
 export default router;
