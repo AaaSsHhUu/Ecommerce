7b0fc42cbc692c9e5511432860662ea405bbf71d
Ashu Negi <anegi6723@gmail.com>
2024-07-10 12:51:20 +0530

    added delete coupon route and auth middleware

--- ecommerce-backend/src/routes/payment.ts
+++ ecommerce-backend/src/routes/payment.ts
@@ -1,12 +1,15 @@
 import express from "express";
-import { allCoupons, applyCoupon, newCoupon } from "../controller/payment.js";
+import { allCoupons, applyCoupon, deleteCoupon, newCoupon } from "../controller/payment.js";
 import { isAdmin, isAuthenticated } from "../middleware/auth.js";
 
 const router = express.Router();
 
-router.post("/coupon/new", newCoupon);
+router.post("/coupon/new",isAuthenticated, isAdmin, newCoupon);
+
 router.post("/discount", applyCoupon);
 
 router.get("/coupon/all",isAuthenticated, isAdmin, allCoupons);
+
+router.delete("/coupon/:id",isAuthenticated, isAdmin, deleteCoupon);
 
 export default router;
