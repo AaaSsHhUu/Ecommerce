4129a611597a2768bcc0eedc5816694ef25a7d58
Ashu Negi <anegi6723@gmail.com>
2024-07-10 13:35:32 +0530

    added dashboard route in app.ts

--- ecommerce-backend/src/app.ts
+++ ecommerce-backend/src/app.ts
@@ -14,6 +14,7 @@ import userRoutes from "./routes/user.js";
 import productRoutes from "./routes/product.js";
 import orderRoutes from "./routes/order.js";
 import paymentRoutes from "./routes/payment.js";
+import dashboardRoutes from "./routes/stats.js"
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -38,7 +39,7 @@ app.use("/api/v1/user", userRoutes);
 app.use("/api/v1/product", productRoutes);
 app.use("/api/v1/order", orderRoutes);
 app.use("/api/v1/payment", paymentRoutes);
-
+app.use("/api/v1/dashboard", dashboardRoutes);
 
 
 app.use("/uploads",express.static("uploads"))
