fc3d03f36ae8e42923966f5986e7bf1b9bd047af
Ashu Negi <anegi6723@gmail.com>
2024-07-16 19:25:24 +0530

    feat: add SignupInfo and LoginInfo interfaces

--- ecommerce-frontend/src/types/types.ts
+++ ecommerce-frontend/src/types/types.ts
@@ -1,3 +1,19 @@
+export interface SignupInfo{
+    name : string;
+    email : string;
+    password : string;
+    photo : string;
+    gender : string;
+    role : string;
+    dob : string;
+    _id : string;   
+}
+
+export interface LoginInfo{
+    email : string;
+    password : string;
+}
+
 export type OrderItemType = {
     _id : string;
     name : string;
@@ -5,6 +21,7 @@ export type OrderItemType = {
     quantity : number;
     photo : string;
 }
+
 export type OrderType = {
     _id : string;
     name : string;
