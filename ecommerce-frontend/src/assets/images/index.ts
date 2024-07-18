1890a4d0e0bf60378825d70246d0262271a7aa27
Ashu Negi <anegi6723@gmail.com>
2024-07-18 19:03:08 +0530

    added logo

--- ecommerce-frontend/src/assets/images/index.ts
+++ ecommerce-frontend/src/assets/images/index.ts
@@ -2,5 +2,6 @@ import banner1 from "./banner1.jpg"
 import banner2 from "./banner2.webp"
 import banner3 from "./banner3.jpg"
 import banner4 from "./banner4.jpg"
+import logo from "./logo.png"
 
-export {banner1,banner2,banner3,banner4}
+export {banner1,banner2,banner3,banner4, logo}
