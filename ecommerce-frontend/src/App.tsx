0cc5725312fbfb7f50e2aa068a10e88547a1684c
Ashu Negi <anegi6723@gmail.com>
2024-07-19 17:53:24 +0530

    used onAuthStateChanged , and reducers to check for current user state

--- ecommerce-frontend/src/App.tsx
+++ ecommerce-frontend/src/App.tsx
@@ -1,10 +1,32 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Suspense } from "react";
-import { BarCharts, Cart, Customer, Dashboard, Home, LineCharts, Login, NewProduct, PieCharts, Product, ProductManagment, Search, Shipping, Signup, TransactionManagment, Transactions } from "./pages";
-import { Header, Loader } from "./components";
-import {Toaster} from "react-hot-toast";
+import { Suspense, useEffect } from "react";
+import { BarCharts, Cart, Customer, Dashboard, Home, LineCharts, Login, NewProduct, PieCharts, Product, ProductManagment, Search, Shipping, TransactionManagment, Transactions } from "./pages";
+import { Footer, Header, Loader } from "./components";
+import { Toaster } from "react-hot-toast";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./firebase";
+import { useDispatch } from "react-redux";
+import { userExist, userNotExist } from "./redux/reducer/userReducer";
+import { getUser } from "./redux/api/userApi";
+
 
 function App() {
+
+  const dispatch = useDispatch();
+  // Firebase provides an onAuthStateChanged method to observe the user's sign-in state. This method takes a callback function that runs whenever the user's authentication state changes.
+  // To check if any user has logged in or not
+  useEffect(() => {
+    onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        // console.log("Logged in user : ", user);
+        const data = await getUser(user.uid);
+        dispatch(userExist(data.user));
+      } else {
+        // console.log("Not logged in");
+        dispatch(userNotExist());
+      }
+    })
+  })
 
   return (
     <Router>
@@ -18,9 +40,8 @@ function App() {
           <Route path="/cart" element={<Cart />} />
           <Route path="/shipping" element={<Shipping />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
 
-        {/* Admin Routes */}
+          {/* Admin Routes */}
           <Route path="/admin/dashboard" element={<Dashboard />} />
           <Route path="/admin/product" element={<Product />} />
           <Route path="/admin/transaction" element={<Transactions />} />
@@ -30,7 +51,7 @@ function App() {
           <Route path="/admin/chart/bar" element={<BarCharts />} />
           <Route path="/admin/chart/pie" element={<PieCharts />} />
           <Route path="/admin/chart/line" element={<LineCharts />} />
-          
+
           {/* Apps */}
 
           {/* Managment */}
@@ -40,7 +61,8 @@ function App() {
 
         </Routes>
       </Suspense>
-      <Toaster position="top-center" />
+      <Footer />
+      <Toaster position="top-center" toastOptions={{ duration: 5000 }} />
     </Router>
   )
 }
