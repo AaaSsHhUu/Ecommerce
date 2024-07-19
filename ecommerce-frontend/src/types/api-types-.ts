a97095418fe40b5791fae8bd73b25805d823bb5c
Ashu Negi <anegi6723@gmail.com>
2024-07-19 17:51:36 +0530

    created type for axios response of single user

--- ecommerce-frontend/src/types/api-types-.ts
+++ ecommerce-frontend/src/types/api-types-.ts
@@ -1,4 +1,11 @@
+import { User } from "./types";
+
 export interface MessageResponse{
     success : boolean;
     message : string;
+}
+
+export type AxiosGetUserResponse = {
+    success : boolean;
+    user : User;
 }
