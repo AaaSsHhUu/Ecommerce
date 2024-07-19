d1ffefdb3e995b90d4687780cf8667262aff2e35
Ashu Negi <anegi6723@gmail.com>
2024-07-19 12:54:38 +0530

    modified header and app.scss styling

--- ecommerce-frontend/src/components/Header.tsx
+++ ecommerce-frontend/src/components/Header.tsx
@@ -1,65 +1,68 @@
-import { useState } from "react"
-import { FaHome, FaSearch, FaShoppingBag, FaSignInAlt, FaSignOutAlt, FaUserCircle } from "react-icons/fa"
-import { IoLogoBitbucket } from "react-icons/io"
-import { Link } from "react-router-dom"
+import { useState } from "react";
+import { FaHome, FaSearch, FaShoppingBag, FaSignInAlt, FaSignOutAlt, FaUserCircle } from "react-icons/fa";
+import { IoLogoBitbucket } from "react-icons/io";
+import { Link } from "react-router-dom";
 
 const Header = () => {
     const users = { _id: "", role: "admin" }
 
-    const [isOpen, setIsOpen] = useState(false);
-
+    const [isDialogOpen, setIsDialogOpen] = useState(false);
     const logoutHandler = () => {
-        setIsOpen(false);
-        // log out logic
+        setIsDialogOpen(false);
     }
-    return (
-        <nav className="header">
-            <div className="logo">
-                <IoLogoBitbucket />
-                <Link to={"/"}><h1>BuyBucket</h1></Link>
-            </div>
-            <div className="nav-links">
-                <Link to={"/"} onClick={() => setIsOpen(false)}>
-                    <FaHome size={"1.3rem"} />Home
-                </Link>
-                <Link to={"/search"} onClick={() => setIsOpen(false)}>
-                    <FaSearch />Search
-                </Link>
-                <Link to={"/cart"} onClick={() => setIsOpen(false)}>
-                    <FaShoppingBag />Cart
-                </Link>
-                {
-                    users?._id ?  // means user is logged in
-                        (
-                            <>
-                                <button onClick={() => setIsOpen(prev => !prev)}>
-                                    <FaUserCircle size={"1.3rem"}/>
-                                </button>
-                                <dialog open={isOpen}>
-                                    <div>
-                                        {
-                                            users?.role === "admin" && (
-                                                <Link to={"/admin/dashboard"} onClick={() => setIsOpen(false)}>Admin</Link>
-                                            )
-                                        }
 
-                                        <Link to={"/orders"}>Orders</Link>
-                                        <button onClick={logoutHandler}>
-                                            <FaSignOutAlt />Logout
-                                        </button>
-                                    </div>
-                                </dialog>
-                            </>
-                        )
-                        :
-                        (
-                            <Link to={"/login"}>
-                                <FaSignInAlt />Login
-                            </Link>
-                        )
-                }
-            </div>
-        </nav>
+    return (
+        <div className="header-container">
+            <nav className="header">
+                <Link to={"/"} className="logo">
+                    <IoLogoBitbucket />
+                    <h1>BuyBucket</h1>
+                </Link>
+                <div className="nav-links">
+                    <Link to={"/"} onClick={() => setIsDialogOpen(false)}>
+                        <FaHome size={"1.3rem"} />Home
+                    </Link>
+                    <Link to={"/search"} onClick={() => setIsDialogOpen(false)}>
+                        <FaSearch />Search
+                    </Link>
+                    <Link to={"/cart"} onClick={() => setIsDialogOpen(false)}>
+                        <FaShoppingBag />Cart
+                    </Link>
+                    {
+                        users?._id ?  // means user is logged in
+                            (
+                                <div className="profile">
+                                    <button onClick={() => setIsDialogOpen(prev => !prev)}>
+                                        <FaUserCircle size={"1.3rem"} />Profile
+                                    </button>
+                                    <dialog open={isDialogOpen} style={{ display: isDialogOpen ? "block" : "none" }}>
+                                        <div>
+                                            {
+                                                users?.role === "admin" && (
+                                                    <Link to={"/admin/dashboard"} onClick={() => setIsDialogOpen(false)}>Admin</Link>
+                                                )
+                                            }
+
+                                            <Link to={"/orders"}>Orders</Link>
+                                            <button onClick={logoutHandler}>
+                                                <FaSignOutAlt />Logout
+                                            </button>
+                                        </div>
+                                    </dialog>
+                                </div>
+                            )
+                            :
+                            (
+                                <>
+                                    <Link to={"/login"}>
+                                        <FaSignInAlt />Login
+                                    </Link>
+                                </>
+                            )
+                    }
+                </div>
+            </nav>
+        </div>
     )
 }
 
