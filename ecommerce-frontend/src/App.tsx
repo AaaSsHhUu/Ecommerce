5414004e27d7b477f87db4ccfdc7b242d1202d71
Ashu Negi <anegi6723@gmail.com>
2024-07-20 18:33:02 +0530

    used Protected route and redux for getting user state

--- ecommerce-frontend/src/App.tsx
+++ ecommerce-frontend/src/App.tsx
@@ -1,17 +1,20 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Suspense, useEffect } from "react";
 import { BarCharts, Cart, Customer, Dashboard, Home, LineCharts, Login, NewProduct, PieCharts, Product, ProductManagment, Search, Shipping, TransactionManagment, Transactions } from "./pages";
-import { Footer, Header, Loader } from "./components";
+import { Header, Loader, ProtectedRoute } from "./components";
 import { Toaster } from "react-hot-toast";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "./firebase";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { userExist, userNotExist } from "./redux/reducer/userReducer";
 import { getUser } from "./redux/api/userApi";
+import { UserReducerInitialState } from "./types/reducer-types";
 
 
 function App() {
-
+  const {user, loading} = useSelector(
+      (state : {userReducer : UserReducerInitialState}
+    ) => state.userReducer)
   const dispatch = useDispatch();
   // Firebase provides an onAuthStateChanged method to observe the user's sign-in state. This method takes a callback function that runs whenever the user's authentication state changes.
   // To check if any user has logged in or not
@@ -26,26 +29,37 @@ function App() {
         dispatch(userNotExist());
       }
     })
-  },[])
+  },[]);
 
   return (
+    loading ? <Loader /> : 
     <Router>
       {/* Header */}
-      <Header />
+      <Header user={user} />
 
       <Suspense fallback={<Loader />}>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/search" element={<Search />} />
           <Route path="/cart" element={<Cart />} />
-          <Route path="/shipping" element={<Shipping />} />
-          <Route path="/login" element={<Login />} />
+          <Route path="/login" element={
+            <ProtectedRoute isAuthenticated={user ? false : true} >
+                  <Login />
+            </ProtectedRoute>
+          } />
+
+          <Route element={<ProtectedRoute isAuthenticated={user ? true : false} /> } >
+              <Route path="/shipping" element={<Shipping />} />
+          </Route>
+
 
           {/* Admin Routes */}
-          <Route path="/admin/dashboard" element={<Dashboard />} />
-          <Route path="/admin/product" element={<Product />} />
-          <Route path="/admin/transaction" element={<Transactions />} />
-          <Route path="/admin/customer" element={<Customer />} />
+          <Route element={<ProtectedRoute adminOnly={true} isAuthenticated={true} admin={user?.role === "admin"} /> }>
+              <Route path="/admin/dashboard" element={<Dashboard />} />
+              <Route path="/admin/product" element={<Product />} />
+              <Route path="/admin/transaction" element={<Transactions />} />
+              <Route path="/admin/customer" element={<Customer />} />
+          </Route>
 
           {/* Charts */}
           <Route path="/admin/chart/bar" element={<BarCharts />} />
@@ -61,7 +75,6 @@ function App() {
 
         </Routes>
       </Suspense>
-      <Footer />
       <Toaster position="top-center" toastOptions={{ duration: 5000 }} />
     </Router>
   )
