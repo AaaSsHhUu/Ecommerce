dcf5fa5deb2dd8cc9d4a8cd1954d5845fd8790fb
Ashu Negi <anegi6723@gmail.com>
2024-07-23 06:50:35 +0530

    created Product loader

--- ecommerce-frontend/src/components/admin/Loader.tsx
+++ ecommerce-frontend/src/components/admin/Loader.tsx
@@ -1,3 +1,5 @@
+import ContentLoader from "react-content-loader";
+
 const Loader = () => {
   return (
     <section className="loader">
@@ -6,13 +8,20 @@ const Loader = () => {
   )
 }
 
-export const Skeleton = () => {
+export const ProductSkeleton = () => {
     return(
-        <div className="skeleton-loader">
-            <div className="skeleton-shape"></div>
-            <div className="skeleton-shape"></div>
-            <div className="skeleton-shape"></div>
-        </div>
+        <ContentLoader
+          speed={1}
+          width={210}
+          height={300}
+          viewBox="0 0 210 300"
+          backgroundColor="#b0b0b0"
+          foregroundColor="#eee"
+        >
+          <rect x="10" y="8" rx="3" ry="3" width="200" height="150" />
+          <rect x="10" y="180" rx="3" ry="3" width="200" height="30" />
+          <rect x="10" y="220" rx="3" ry="3" width="200" height="30" />
+        </ContentLoader>
     )
 }
 
