914ff9491f0b93350f0e0ee90a087f30292db9ca
Ashu Negi <anegi6723@gmail.com>
2024-07-28 06:45:38 +0530

    adding auth middleware

--- ecommerce-backend/src/routes/user.ts
+++ ecommerce-backend/src/routes/user.ts
@@ -7,6 +7,6 @@ const router = express.Router();
 router.post("/new", createUser);
 
 router.get("/all",isAuthenticated, isAdmin ,getAllUsers);
-router.route("/:id").get(getUser).delete(isAdmin, deleteUser).put(isAdmin, updateUserProfile);
+router.route("/:id").get(getUser).delete(isAuthenticated, isAdmin, deleteUser).put(isAuthenticated, isAdmin, updateUserProfile);
 
 export default router;
