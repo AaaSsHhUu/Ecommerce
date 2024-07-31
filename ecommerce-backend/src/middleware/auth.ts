94408cef4462224ae4d11317aacd74ed2d4c5862
Ashu Negi <anegi6723@gmail.com>
2024-07-31 05:48:21 +0530

    changed auth middleware, using id from query

--- ecommerce-backend/src/middleware/auth.ts
+++ ecommerce-backend/src/middleware/auth.ts
@@ -1,38 +1,23 @@
 import { NextFunction, Request, Response } from "express";
 import ErrorHandler from "../utils/errorHandler.js"
-import jwt from "jsonwebtoken";
 import User from "../models/user.js";
-import { IUser } from "../types/types.js";
 
-export const isAuthenticated = async (req : Request,res : Response,next : NextFunction) => {
-        try {
-            const { token } = req.cookies;
+export const adminOnly = 
+    async (req : Request,res : Response,next : NextFunction) => {
+        const {id} = req.query;
 
-            if(!token){
-                throw new ErrorHandler("Authentication token required",401);
-            }
-
-            const decodedData = await jwt.verify(token,process.env.JWT_SECRET as string) as {id : string};
-            // console.log("decoded data : ", decodedData);
-            
-            const user = await User.findById(decodedData.id).select("-password");
-            // console.log("user after authentication : ",user);
-            
-            req.user = user as IUser;
-            // console.log("req.user", req.user);
-            
-            return next();
-        }catch (error) {
-           return next(new ErrorHandler("Authentication Error : " + error,400)) ;
+        if(!id){
+            throw new ErrorHandler("Please Login First", 401);
         }
-}
 
-export const isAdmin = async (req : Request, res : Response, next : NextFunction) => {
-        if(!req.user){
-            return next(new ErrorHandler("You are not logged in",400))
+        const user = await User.findById(id);
+
+        if(!user){
+            throw new ErrorHandler("User doesn't exist, Please Signup first", 401);
         }
-        if(req.user.role !== "admin"){
-            return next(new ErrorHandler("You are not allowed to access this page",403));
+        if(user.role !== "admin"){
+            throw new ErrorHandler("You can't access this page", 403);
         }
+
         return next();
     }
