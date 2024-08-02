5329f7a517c07773e0cf4a4bce5ae8e62417ee03
Ashu Negi <anegi6723@gmail.com>
2024-08-02 12:38:50 +0530

    added cartReducer to store

--- ecommerce-frontend/src/redux/store.ts
+++ ecommerce-frontend/src/redux/store.ts
@@ -2,6 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import { userAPI } from "./api/userApi";
 import { userReducer } from "./reducer/userReducer";
 import { productApi } from "./api/productApi";
+import { cartReducer } from "./reducer/cartReducer";
 
 
 export const server = import.meta.env.VITE_SERVER;
@@ -11,7 +12,8 @@ export const store = configureStore({
         [userAPI.reducerPath] : userAPI.reducer,
         [productApi.reducerPath] : productApi.reducer,
 
-        [userReducer.name] : userReducer.reducer
+        [userReducer.name] : userReducer.reducer,
+        [cartReducer.name] : cartReducer.reducer
     },
     middleware : (defaultMiddlewares) => defaultMiddlewares().concat(userAPI.middleware, productApi.middleware)
 })
