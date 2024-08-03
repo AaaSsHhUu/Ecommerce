8d2e8d8f81ae02fd3ab17fd030e1efd3ff0d9d2f
Ashu Negi <anegi6723@gmail.com>
2024-08-03 22:19:25 +0530

    added queries for orderApi

--- ecommerce-frontend/src/redux/api/orderApi.ts
+++ ecommerce-frontend/src/redux/api/orderApi.ts
@@ -1,21 +1,36 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { MessageResponse, NewOrderRequest } from "../../types/api-types-";
+import { AllOrdersResponse, MessageResponse, NewOrderRequest } from "../../types/api-types-";
 
 export const orderAPI = createApi({
     reducerPath : "orderApi",
     baseQuery : fetchBaseQuery({ 
         baseUrl : `${import.meta.env.VITE_SERVER}/api/v1/order`
     }),
+    tagTypes : ["orders"],
     endpoints : (builder) => ({
+        
         newOrder : builder.mutation<MessageResponse, NewOrderRequest>({
             query : (order) => ({
                 url : "/new",
                 method : "POST",
                 body : order
-            })
+            }),
+            invalidatesTags : ["orders"]
         }),
-        myOrder : builder.query<MessageResponse, string>({
-            query : (id) => (`/my-order/${id}`)
+
+        myOrder : builder.query<AllOrdersResponse, string>({
+            query : (id) => (`/my-order/${id}`),
+            providesTags : ["orders"]
+        }),
+        
+        allOrders : builder.query<AllOrdersResponse,string>({
+            query : (id) => (`/all-orders?id=${id}`),
+            providesTags : ["orders"]
+        }),
+
+        orderDetails : builder.query<AllOrdersResponse, string>({
+            query : (id) => (`/${id}`),
+            providesTags : ["orders"]
         })
     })
 })
