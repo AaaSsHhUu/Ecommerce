a8a1d387ec13e85d3b098a795e280975c5bc3e84
Ashu Negi <anegi6723@gmail.com>
2024-08-03 12:28:51 +0530

    used calculatePrice reducer method in cart

--- ecommerce-frontend/src/pages/Cart.tsx
+++ ecommerce-frontend/src/pages/Cart.tsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { CartReducerInitialState } from "../types/reducer-types";
 import { CartItem } from "../types/types";
-import { addToCart, removeCartItem } from "../redux/reducer/cartReducer";
+import { addToCart, calculatePrice, removeCartItem } from "../redux/reducer/cartReducer";
 import toast from "react-hot-toast";
 
 const Cart = () => {
@@ -17,59 +17,64 @@ const Cart = () => {
     total,
     discount,
     shippingCharges,
-  } = useSelector(( state : {cartReducer : CartReducerInitialState} ) => state.cartReducer);
-  
+  } = useSelector((state: { cartReducer: CartReducerInitialState }) => state.cartReducer);
+
   const [couponCode, setCouponCode] = useState<string>("");
   const [isValidCoupon, setIsValidCoupon] = useState<boolean>(false);
 
   const dispatch = useDispatch();
 
   useEffect(() => {
-    if(couponCode === "Ashu2004"){
+    if (couponCode === "Ashu2004") {
       setIsValidCoupon(true);
     }
-    else{
+    else {
       setIsValidCoupon(false);
     }
-  },[couponCode])
+  }, [couponCode])
 
-  const removeCartItemHandler = (productId : string) => {
+
+  useEffect(() => {
+    dispatch(calculatePrice());
+  },[cartItems])
+
+  const removeCartItemHandler = (productId: string) => {
     dispatch(removeCartItem(productId));
     toast.success("Item Removed");
-}
+  }
 
-const increaseQuantity = (cartItem : CartItem) => {
-    if(cartItem.quantity <  cartItem.stock){
-        dispatch(addToCart({...cartItem, quantity : cartItem.quantity + 1}));
+  const increaseQuantity = (cartItem: CartItem) => {
+    if (cartItem.quantity < cartItem.stock) {
+      dispatch(addToCart({ ...cartItem, quantity: cartItem.quantity + 1 }));
     }
-    else{
-        return toast.error("Out of Stock");
+    else {
+      return toast.error("Out of Stock");
     }
-}
+  }
 
-const decreaseQuantity = (cartItem : CartItem) => {
-    if(cartItem.quantity > 1){
-        dispatch(addToCart({...cartItem, quantity : cartItem.quantity - 1}));
+  const decreaseQuantity = (cartItem: CartItem) => {
+    if (cartItem.quantity > 1) {
+      dispatch(addToCart({ ...cartItem, quantity: cartItem.quantity - 1 }));
     }
-    else{
-        return toast.error("Quantity cannot be less than 1");
+    else {
+      return toast.error("Quantity cannot be less than 1");
     }
-}
+  }
 
   return (
     <div className="cart">
       <main>
         {
-          cartItems.length > 0 ? 
-          (
-            cartItems.map((item,idx) => (
-            <CartItemCard key={idx} cartItem={item} increaseQuantity={increaseQuantity} decreaseQuantity={decreaseQuantity} removeCartItemHandler={removeCartItemHandler} />
-            ))
-          )
-          :
-          (
-            <h1>Cart is Empty</h1>
-          )
+          cartItems.length > 0 ?
+            (
+              cartItems.map((item, idx) => (
+                <CartItemCard key={idx} cartItem={item} increaseQuantity={increaseQuantity} decreaseQuantity={decreaseQuantity} removeCartItemHandler={removeCartItemHandler} />
+              ))
+            )
+            :
+            (
+              <h1>Cart is Empty</h1>
+            )
         }
       </main>
 
@@ -87,21 +92,21 @@ const decreaseQuantity = (cartItem : CartItem) => {
         />
 
         {
-          couponCode && 
+          couponCode &&
           (
             isValidCoupon ?
-            <span className="green">
-              ₹{discount} off using the <code>{couponCode}</code>
-            </span>
-            :
-            <span className="red">
-              Invalid Coupon <VscError />
-            </span>
+              <span className="green">
+                ₹{discount} off using the <code>{couponCode}</code>
+              </span>
+              :
+              <span className="red">
+                Invalid Coupon <VscError />
+              </span>
           )
         }
 
         {
-          cartItems.length > 0 && 
+          cartItems.length > 0 &&
           <Link to={"/shipping"} className="checkout-btn">Checkout</Link>
         }
       </aside>
