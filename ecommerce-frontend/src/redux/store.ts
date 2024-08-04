e15e36da3960933481753e3a49bf898b341ce4d3
Ashu Negi <anegi6723@gmail.com>
2024-08-04 19:23:17 +0530

    added orderApi reducer and middleware in the store

--- ecommerce-frontend/src/redux/store.ts
+++ ecommerce-frontend/src/redux/store.ts
@@ -3,6 +3,7 @@ import { userAPI } from "./api/userApi";
 import { userReducer } from "./reducer/userReducer";
 import { productApi } from "./api/productApi";
 import { cartReducer } from "./reducer/cartReducer";
+import { orderAPI } from "./api/orderApi";
 
 
 export const server = import.meta.env.VITE_SERVER;
@@ -11,11 +12,12 @@ export const store = configureStore({
     reducer : {
         [userAPI.reducerPath] : userAPI.reducer,
         [productApi.reducerPath] : productApi.reducer,
+        [orderAPI.reducerPath] : orderAPI.reducer,
 
         [userReducer.name] : userReducer.reducer,
         [cartReducer.name] : cartReducer.reducer
     },
-    middleware : (defaultMiddlewares) => defaultMiddlewares().concat(userAPI.middleware, productApi.middleware)
+    middleware : (defaultMiddlewares) => defaultMiddlewares().concat(userAPI.middleware, productApi.middleware, orderAPI.middleware)
 })
 
 export type RootState = ReturnType<typeof store.getState>
