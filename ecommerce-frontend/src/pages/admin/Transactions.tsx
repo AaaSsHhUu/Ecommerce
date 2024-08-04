604b3964c017634f1cbb6d159a31aeacc6b4f737
Ashu Negi <anegi6723@gmail.com>
2024-08-04 19:24:12 +0530

    using useAllOrderQuery to fetch all orders

--- ecommerce-frontend/src/pages/admin/Transactions.tsx
+++ ecommerce-frontend/src/pages/admin/Transactions.tsx
@@ -1,46 +1,82 @@
-import { ReactElement, useState } from "react";
+import { ReactElement, useEffect, useState } from "react";
 import { AdminSidebar } from "../../components"
 import { Column } from "react-table";
 import Table from "../../components/admin/Table";
+import { useSelector } from "react-redux";
+import { UserReducerInitialState } from "../../types/reducer-types";
+import { useAllOrdersQuery } from "../../redux/api/orderApi";
+import { CustomError } from "../../types/api-types-";
+import toast from "react-hot-toast";
+import { Link } from "react-router-dom";
+
+interface DataType{
+  user : string;
+  amount : number;
+  discount : number;
+  quantity : number;
+  status : ReactElement;
+  action : ReactElement;
+}
+
+const columns : Column<DataType>[] = [
+  {
+    Header : "Users",
+    accessor : "user"
+  },
+  {
+    Header : "Amount",
+    accessor : "amount"
+  },
+  {
+    Header : "Discount",
+    accessor : "discount"
+  },
+  {
+    Header : "Quantity",
+    accessor : "quantity"
+  },
+  {
+    Header : "Status",
+    accessor : "status"
+  },
+  {
+    Header : "Action",
+    accessor : "action"
+  },
+]
 
 const Transactions = () => {
-  interface DataType{
-    user : string;
-    amount : number;
-    discount : number;
-    quantity : number;
-    status : ReactElement;
-    action : ReactElement;
-  }
+
+  const {user} = useSelector((state : {userReducer : UserReducerInitialState}) => state.userReducer);
+
+  const {isLoading, data, isError, error} = useAllOrdersQuery(user?._id!);
 
   const [rows, setRows] = useState<DataType[]>([]);
 
-  const columns : Column<DataType>[] = [
-    {
-      Header : "Users",
-      accessor : "user"
-    },
-    {
-      Header : "Amount",
-      accessor : "amount"
-    },
-    {
-      Header : "Discount",
-      accessor : "discount"
-    },
-    {
-      Header : "Quantity",
-      accessor : "quantity"
-    },
-    {
-      Header : "Status",
-      accessor : "status"
-    },
-    {
-      Header : "Action",
-      accessor : "action"
-    },
-  ]
+  if(isError){
+    const err = error as CustomError;
+    return toast.error(err.data.message);
+  }
+  console.log("data : ", data);
+  
+  useEffect(() => {
+    if(data?.orders.length){
+      setRows(
+        data.orders.map((i) => ({
+          user : i.user.name,
+          amount : i.total,
+          discount : i.discount,
+          quantity : i.orderItems.length,
+          status : <span className={
+              i.status === "Processing" ? "red" 
+              : i.status === "Shipped" ? "green" 
+              : "purple"
+            }>{i.status}</span>,
+          action : <Link to={`/admin/transaction/${i._id}`}>Manage</Link>
+        }))
+      )
+    }
+  },[data])
 
   return (
     <div className="admin-container">
