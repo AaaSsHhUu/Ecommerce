33685967427aea0b17586599b38f2522acf4d14e
Ashu Negi <anegi6723@gmail.com>
2024-08-05 19:09:44 +0530

    fetching orderDetails using useOrderDetailsQuery

--- ecommerce-frontend/src/pages/admin/managment/TransactionManagment.tsx
+++ ecommerce-frontend/src/pages/admin/managment/TransactionManagment.tsx
@@ -1,104 +1,130 @@
-import { useState } from "react"
-import { AdminSidebar } from "../../../components"
-import { Link } from "react-router-dom";
-import { OrderItemType, OrderType } from "../../../types/types"
+import { useSelector } from "react-redux";
+import { Link, Navigate, useParams } from "react-router-dom";
+import { AdminSidebar, TableSkeleton } from "../../../components";
+import { useOrderDetailsQuery } from "../../../redux/api/orderApi";
+import { UserReducerInitialState } from "../../../types/reducer-types";
+import { Order, OrderItemType } from "../../../types/types";
 
 const TransactionManagment = () => {
 
-  const orderItems: OrderItemType[] = [];
+  const { user } = useSelector((state: { userReducer: UserReducerInitialState }) => state.userReducer);
 
-  const [order, setOrder] = useState<OrderType>({
-    _id: "smkdcmowi",
-    name: "Nike Shoes",
-    address: "77 White Street",
-    city: "New-Delhi",
-    state: "Phulera",
-    country: "India",
-    pinCode: 263661,
+  const {id : orderId} = useParams();
+  console.log("order id : ",orderId);
+  
+  const { data, isLoading, isError, error } = useOrderDetailsQuery(orderId as string);
+
+  const defaultData: Order = {
+    shippingInfo: {
+      address: "77 White Street",
+      city: "New-Delhi",
+      state: "Phulera",
+      country: "India",
+      pinCode: "263661",
+    },
     status: "Processing",
-    subTotal: 4000,
-    discount: 1200,
-    tax: 200,
+    subTotal: 0,
+    discount: 0,
+    tax: 0,
     shippingCharges: 0,
-    total: 4000 + 200 + 0 - 1200,
-    orderItems
-  });
+    total: 0,
+    orderItems: [],
+    user: {
+      _id: "",
+      name: ""
+    },
+    _id: ""
+  }
+console.log("Data : ", data);
 
   const {
-    name,
-    address,
-    city,
-    country,
-    state,
-    pinCode,
+    shippingInfo: {
+      address,
+      city,
+      state,
+      country,
+      pinCode
+    },
+    orderItems,
+    status,
     subTotal,
-    shippingCharges,
     discount,
     tax,
+    shippingCharges,
     total,
-    status
-  } = order;
+    user: { name },
+
+  } = data?.order || defaultData;
 
   const updateHandler = () => {
-    
+
   }
 
   const deleteHandler = () => {
 
   }
 
+  console.log("error : ", error);
+
+  if (isError) <Navigate to={"/404"} />
+
   return (
     <>
       <div className="admin-container">
-          <AdminSidebar />
-          <main className="product-managment">
-            <section>
-              <h2>Order Items</h2>
+        <AdminSidebar />
+        <main className="product-managment">
+          {isLoading ? <TableSkeleton />
+            :
+            <>
+              <section>
+                <h2>Order Items</h2>
 
-              {order.orderItems.map((item) => (
-                <OrderItemCard 
-                  name={item.name}
-                  photo={item.photo} 
-                  price={item.price} 
-                  quantity={item.quantity} 
-                  _id={item._id} 
-                />
-              ))}
-            </section>
+                {orderItems.map((item) => (
+                  <OrderItemCard
+                    name={item.name}
+                    photo={item.photo}
+                    price={item.price}
+                    quantity={item.quantity}
+                    _id={item._id}
+                  />
+                ))}
+              </section>
 
-            <article className="shipping-info-card">
-              <h1>Order Info</h1>
-              <h5>User Info</h5>
-              <p>Name : {name}</p>
-              <p>
-                Address : {`
+              <article className="shipping-info-card">
+                <h1>Order Info</h1>
+                <h5>User Info</h5>
+                <p>Name : {name}</p>
+                <p>
+                  Address : {`
                             ${address},
                             ${city}, ${state},
                             ${country} - ${pinCode}
                           `}
-              </p>
+                </p>
 
-              <h5>Amount Info</h5>
-              <p>Subtotal : {subTotal}</p>
-              <p>Shipping charges : {shippingCharges}</p>
-              <p>Tax : {tax}</p>
-              <p>Discount : {discount}</p>
-              <p>Total : {total}</p>
+                <h5>Amount Info</h5>
+                <p>Subtotal : {subTotal}</p>
+                <p>Shipping charges : {shippingCharges}</p>
+                <p>Tax : {tax}</p>
+                <p>Discount : {discount}</p>
+                <p>Total : {total}</p>
 
-              <h5>Status Info</h5>
-              <p>Status : &nbsp;
-                <span
-                  className={
-                    status === "Delivered" ? "purple" : 
-                    status === "Shipped" ? "green" : "red"
-                  }
-                >{status}</span>
-              </p>
+                <h5>Status Info</h5>
+                <p>Status : &nbsp;
+                  <span
+                    className={
+                      status === "Delivered" ? "purple" :
+                        status === "Shipped" ? "green" : "red"
+                    }
+                  >{status}</span>
+                </p>
 
-              <button onClick={updateHandler} >Process Status</button>
-              <button onClick={deleteHandler} >Delete Order</button>
-            </article>
-          </main>
+                <button onClick={updateHandler} >Process Status</button>
+                <button onClick={deleteHandler} >Delete Order</button>
+              </article>
+            </>
+          }
+        </main>
       </div>
     </>
   )
