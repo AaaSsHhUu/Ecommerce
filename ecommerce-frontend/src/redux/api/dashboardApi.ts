1071bc38b32b6187d2697411ef8149727332edff
Ashu Negi <anegi6723@gmail.com>
2024-08-08 12:44:35 +0530

    feat : added queries for getting stats data, bar, line and pie chart data

--- ecommerce-frontend/src/redux/api/dashboardApi.ts
+++ ecommerce-frontend/src/redux/api/dashboardApi.ts
@@ -3,11 +3,26 @@ import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
 export const dashboardApi = createApi({
     reducerPath : "dashboardApi",
     baseQuery : fetchBaseQuery({
-        baseUrl : `${import.meta.env.VITE_SERVER}/api/v1/admin`
+        baseUrl : `${import.meta.env.VITE_SERVER}/api/v1/dashboard`
     }),
     endpoints : (builder) => ({
+        
+        stats : builder.query({
+            query : (id) => (`/stats?id=${id}`)
+        }),
 
+        pie : builder.query({
+            query : (id) => (`/pie?id=${id}`)
+        }),
+
+        bar : builder.query({
+            query : (id) => (`/bar?id=${id}`)
+        }),
+
+        line : builder.query({
+            query : (id) => (`/line?id=${id}`)
+        })
     })
 })
 
-export const {} = dashboardApi;
+export const {useBarQuery, usePieQuery, useLineQuery, useStatsQuery} = dashboardApi;
