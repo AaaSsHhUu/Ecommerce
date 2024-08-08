1f4129f2ba06ebf461b1c4e570e68dca86aec2b2
Ashu Negi <anegi6723@gmail.com>
2024-08-08 13:09:24 +0530

    created types for dashboard stats

--- ecommerce-frontend/src/types/types.ts
+++ ecommerce-frontend/src/types/types.ts
@@ -1,83 +1,109 @@
 export type User = {
-    name: string;
-    email: string;
-    photo: string;
-    gender: string;
-    role: string;
-    dob: string;
-    _id: string;
+  name: string;
+  email: string;
+  photo: string;
+  gender: string;
+  role: string;
+  dob: string;
+  _id: string;
 };
 
 export type Product = {
-    name : string;
-    price : number;
-    stock : number;
-    category : string;
-    photo : string;
-    _id : string;
-}
+  name: string;
+  price: number;
+  stock: number;
+  category: string;
+  photo: string;
+  _id: string;
+};
 
 export type OrderItemType = {
-    _id : string;
-    name : string;
-    price : number;
-    quantity : number;
-    photo : string;
-}
+  _id: string;
+  name: string;
+  price: number;
+  quantity: number;
+  photo: string;
+};
 
 export type OrderType = {
-    _id : string;
-    name : string;
-    address : string;
-    city : string;
-    state : string;
-    country : string;
-    pinCode : number;
-    status : "Shipped" | "Processing" | "Delivered";
-    subTotal : number;
-    discount : number;
-    shippingCharges : number;
-    tax : number;
-    total : number;
-    orderItems : OrderItemType[]
-}
+  _id: string;
+  name: string;
+  address: string;
+  city: string;
+  state: string;
+  country: string;
+  pinCode: number;
+  status: "Shipped" | "Processing" | "Delivered";
+  subTotal: number;
+  discount: number;
+  shippingCharges: number;
+  tax: number;
+  total: number;
+  orderItems: OrderItemType[];
+};
 
 export type ShippingInfo = {
-    address : string;
-    city : string;
-    state : string;
-    country : string;
-    pinCode : string;
-}
+  address: string;
+  city: string;
+  state: string;
+  country: string;
+  pinCode: string;
+};
 
 export type CartItem = {
-    productId : string;
-    photo : string;
-    name : string;
-    price : number;
-    quantity : number;
-    stock : number;
-}
+  productId: string;
+  photo: string;
+  name: string;
+  price: number;
+  quantity: number;
+  stock: number;
+};
 
-export type OrderItem = Omit<CartItem, "stock"> & { _id : string }
+export type OrderItem = Omit<CartItem, "stock"> & { _id: string };
 
 export type Order = {
-    orderItems : OrderItem[];
-    shippingInfo : ShippingInfo;
-    subTotal : number;
-    tax : number;
-    discount : number;
-    shippingCharges : number;
-    total : number;
-    status : string;
-    user : {
-        name : string;
-        _id : string;
-    };
-    _id : string;
+  orderItems: OrderItem[];
+  shippingInfo: ShippingInfo;
+  subTotal: number;
+  tax: number;
+  discount: number;
+  shippingCharges: number;
+  total: number;
+  status: string;
+  user: {
+    name: string;
+    _id: string;
+  };
+  _id: string;
+};
+
+type CountAndPercentageChange = {
+    revenue: number;
+    products: number;
+    users: number;
+    orders: number;
 }
 
-export type UpdateOrderRequest = {
-    userId : string;
-    orderId : string;
+type LatestTransaction = {
+    _id : string;
+    amount : number;
+    discount : number;
+    quantity : number;
+    status : string;
 }
+
+
+export type StatsType = {
+  categoryCount : Record<string, number>[];
+  percentage : CountAndPercentageChange;
+  count : CountAndPercentageChange;
+  chart: {
+    order: number[];
+    revenue: number[];
+  };
+  userRatio : {
+    male : number;
+    female : number;
+  }
+ latestTransaction: LatestTransaction[];
+};
