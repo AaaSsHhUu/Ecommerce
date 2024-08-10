9a9805bb766b4617863b5221ff395c9a933c453d
Ashu Negi <anegi6723@gmail.com>
2024-08-10 20:02:25 +0530

    giving types to chart queries

--- ecommerce-frontend/src/redux/api/dashboardApi.ts
+++ ecommerce-frontend/src/redux/api/dashboardApi.ts
@@ -1,5 +1,5 @@
 import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
-import { StatsResponse } from "../../types/api-types-";
+import { BarResponse, LineResponse, PieResponse, StatsResponse } from "../../types/api-types-";
 
 export const dashboardApi = createApi({
     reducerPath : "dashboardApi",
@@ -12,15 +12,15 @@ export const dashboardApi = createApi({
             query : (id) => (`/stats?id=${id}`)
         }),
 
-        pie : builder.query({
+        pie : builder.query<PieResponse, string>({
             query : (id) => (`/pie?id=${id}`)
         }),
 
-        bar : builder.query({
+        bar : builder.query<BarResponse, string>({
             query : (id) => (`/bar?id=${id}`)
         }),
 
-        line : builder.query({
+        line : builder.query<LineResponse, string>({
             query : (id) => (`/line?id=${id}`)
         })
     })
