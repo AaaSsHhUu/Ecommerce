7e36f6c78096c0e6c4651e4a8d0e10377d97ee4e
Ashu Negi <anegi6723@gmail.com>
2024-08-11 15:48:37 +0530

    putting dynamic data in bar chart

--- ecommerce-frontend/src/pages/admin/charts/BarCharts.tsx
+++ ecommerce-frontend/src/pages/admin/charts/BarCharts.tsx
@@ -1,52 +1,66 @@
-import { AdminSidebar, BarChart } from "../../../components"
+import { useSelector } from "react-redux";
+import { AdminSidebar, BarChart, Loader } from "../../../components";
+import { RootState } from "../../../redux/store";
+import { useBarQuery } from "../../../redux/api/dashboardApi";
+import { CustomError } from "../../../types/api-types-";
+import toast from "react-hot-toast";
+import { getLastMonths } from "../../../utils/features";
 
-const months = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December"
-]
+
 const BarCharts = () => {
+
+  const { user: adminUser } = useSelector((state: RootState) => state.userReducer);
+
+  const { isLoading, data, isError, error } = useBarQuery(adminUser?._id!);
+
+  const {lastSixMonths, lastTwelveMonths} = getLastMonths();
+
+  const order = data?.charts.order;
+  const product = data?.charts.product;
+  const user = data?.charts.user;
+
+  if (isError) {
+    const err = error as CustomError;
+    toast.error(err.data.message);
+  }
+
   return (
     <>
       <div className="admin-container">
         <AdminSidebar />
-        <main className="chart-container">
-            <h1>Bar Charts</h1>
-            <section>
-              <BarChart 
-                data1={[200,444,356,764,565,408,908]} 
-                data2={[345,104,412,683,250,786,167]} 
-                title1="Products" 
-                title2="Users" 
-                bgColor1="hsl(260,50%,30%)" 
-                bgColor2="hsl(360,90%,90%)" 
-              />
-              <h2>Top Selling Products & Top Customers</h2>
-            </section>
+        {
+          isLoading ? <Loader />
+            :
+            <main className="chart-container">
+              <h1>Bar Charts</h1>
+              <section>
+                <BarChart
+                  data1={product || []}
+                  data2={user || []}
+                  title1="Products"
+                  title2="Users"
+                  bgColor1="hsl(260,50%,30%)"
+                  bgColor2="hsl(360,90%,90%)"
+                  labels={lastSixMonths}
+                />
+                <h2>Top Selling Products & Top Customers</h2>
+              </section>
 
-            <section>
-              <BarChart 
-                data1={[200,444,356,764,565,408,908,345,104,412,683,250]} 
-                data2={[]} 
-                title1="Products" 
-                title2="" 
-                bgColor1="hsl(180,40%,50%)" 
-                bgColor2=""
-                labels={months} 
-                horizontal={true}
-              />
-              <h2>Orders Throughout the Year</h2>
-            </section>
-        </main>
+              <section>
+                <BarChart
+                  data1={order || []}
+                  data2={[]}
+                  title1="Products"
+                  title2=""
+                  bgColor1="hsl(180,40%,50%)"
+                  bgColor2=""
+                  labels={lastTwelveMonths}
+                  horizontal={true}
+                />
+                <h2>Orders Throughout the Year</h2>
+              </section>
+            </main>
+        }
       </div>
     </>
   )
