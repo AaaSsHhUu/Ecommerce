20783ed231a16e648edcdb7d80a915b763e97d62
Ashu Negi <anegi6723@gmail.com>
2024-09-03 15:23:55 +0530

    added couponapi to store

--- ecommerce-frontend/src/redux/store.ts
+++ ecommerce-frontend/src/redux/store.ts
@@ -5,21 +5,24 @@ import { productApi } from "./api/productApi";
 import { cartReducer } from "./reducer/cartReducer";
 import { orderAPI } from "./api/orderApi";
 import { dashboardApi } from "./api/dashboardApi";
+import { couponApi } from "./api/couponApi";
 
 
 export const server = import.meta.env.VITE_SERVER;
 
 export const store = configureStore({
     reducer : {
+        // Api
         [userAPI.reducerPath] : userAPI.reducer,
         [productApi.reducerPath] : productApi.reducer,
         [orderAPI.reducerPath] : orderAPI.reducer,
         [dashboardApi.reducerPath] : dashboardApi.reducer,
-
+        [couponApi.reducerPath] : couponApi.reducer,
+        // Reducer
         [userReducer.name] : userReducer.reducer,
         [cartReducer.name] : cartReducer.reducer
     },
-    middleware : (defaultMiddlewares) => defaultMiddlewares().concat(userAPI.middleware, productApi.middleware, orderAPI.middleware, dashboardApi.middleware)
+    middleware : (defaultMiddlewares) => defaultMiddlewares().concat(userAPI.middleware, productApi.middleware, orderAPI.middleware, dashboardApi.middleware, couponApi.middleware)
 })
 
 export type RootState = ReturnType<typeof store.getState>
